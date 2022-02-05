bc11f0140f9a713ece4921f452a4375c7b3e6eb8
Aalap Rawat <aalaprawatdurg@gmail.com>
2022-02-05 13:40:12 +0530

    added comments

--- app/src/main/java/in/aalap/gitxplorer/network/Urls.kt
+++ app/src/main/java/in/aalap/gitxplorer/network/Urls.kt
@@ -8,6 +8,9 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Url
 
+/**
+ * This are all the urls
+ */
 
 interface Urls {
     @GET("https://api.github.com/users/{userName}")
